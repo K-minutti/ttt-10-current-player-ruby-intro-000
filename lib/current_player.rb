ea4aaa4bd380a4b0560233108e1f9e60cbe0dabf
Kevin Minutti <minutti70@gmail.com>
2020-03-02 16:45:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,25 +3,24 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   $counter = 0
 def turn_count(board)
   $counter = 0
-  if board.each {|position| position == "X" || "0" 
+  if board.each {|position| position == "X" || "0"
      $counter += 1}
-  return 
+  return
     $counter
 end
 
 # We will need to iterate over each member of the board array
 # check to see if that element is an "X" or an "O". If it is, we increment our counter variable by 1.
-#return value of turn_count should be number of turns that have been played 
+#return value of turn_count should be number of turns that have been played
 
 
 def current_player(board_game)
-  if $counter.even? 
-  return 
+  if $counter.even?
+  return
    "X"
   else
-  return 
+  return
   "O"
-  end 
+  end
 end
 #If the turn count is an even number, the #current_player method should return "X", otherwise, it should return "O".
-
